7dfdcc6700d877e8249280237d64aafe448a9de7
Yannick Garcia <yannick.garcia@gmail.com>
2016-10-07 20:40:46 +0200

    added material and bottom nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Navigation from './components/navigation';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload dude.
-        </p>
+        <MuiThemeProvider>
+          <div style={{position: 'absolute', bottom: '0'}}>
+           <Navigation />
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
---
+++ src/components/navigation.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import FontIcon from 'material-ui/FontIcon';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import IconStar from 'material-ui/svg-icons/action/stars';
+import IconList from 'material-ui/svg-icons/action/list';
+import IconGroup from 'material-ui/svg-icons/action/group-work';
+import IconAccount from 'material-ui/svg-icons/action/account-circle';
+
+const gamesIcon = <IconList />;
+const rankingIcon = <IconStar />;
+const groupIcon = <IconGroup />;
+const profileIcon = <IconAccount />;
+
+/**
+ * A simple example of `BottomNavigation`, with three labels and icons
+ * provided. The selected `BottomNavigationItem` is determined by application
+ * state (for instance, by the URL).
+ */
+class Navigation extends Component {
+  state = {
+    selectedIndex: 0,
+  };
+
+  select = (index) => this.setState({selectedIndex: index});
+
+  render() {
+    return (
+      <Paper zDepth={1}>
+        <BottomNavigation selectedIndex={this.state.selectedIndex}>
+          <BottomNavigationItem
+            label="Games"
+            icon={gamesIcon}
+            onTouchTap={() => this.select(0)}
+          />
+          <BottomNavigationItem
+            label="Ranking"
+            icon={rankingIcon}
+            onTouchTap={() => this.select(1)}
+          />
+          <BottomNavigationItem
+            label="Group"
+            icon={groupIcon}
+            onTouchTap={() => this.select(2)}
+          />
+          <BottomNavigationItem
+            label="Profile"
+            icon={profileIcon}
+            onTouchTap={() => this.select(3)}
+          />
+        </BottomNavigation>
+      </Paper>
+    );
+  }
+}
+
+export default Navigation;
