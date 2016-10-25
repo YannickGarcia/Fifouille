f977ab1b34e29d12545f026f85137fe220c62d8a
Yannick Garcia <yannick.garcia@gmail.com>
2016-10-25 14:19:12 +0200

    draw issue

--- src/firebase-ref.js
+++ src/firebase-ref.js
@@ -1,13 +1,25 @@
 import * as firebase from 'firebase';
 
 // Initialize Firebase
-const config = {
-    apiKey: "AIzaSyCQ-Ki-mSuqsRbqsMfXOUTDySBqYW3xWgs",
-    authDomain: "fifouille-5dccb.firebaseapp.com",
-    databaseURL: "https://fifouille-5dccb.firebaseio.com",
-    storageBucket: "fifouille-5dccb.appspot.com",
-    messagingSenderId: "671212363239"
-};
+
+/* // DB PROD
+ const config = {
+ apiKey: "AIzaSyCQ-Ki-mSuqsRbqsMfXOUTDySBqYW3xWgs",
+ authDomain: "fifouille-5dccb.firebaseapp.com",
+ databaseURL: "https://fifouille-5dccb.firebaseio.com",
+ storageBucket: "fifouille-5dccb.appspot.com",
+ messagingSenderId: "671212363239"
+ };
+ */
+
+// DB DEV
+ const config = {
+     apiKey: "AIzaSyBRp1ebUwX98lKMK6ns8i9EEY6M2twy1oo",
+     authDomain: "offifa-dev.firebaseapp.com",
+     databaseURL: "https://offifa-dev.firebaseio.com",
+     storageBucket: "offifa-dev.appspot.com",
+     messagingSenderId: "733443285053"
+ };
 
 
 firebase.initializeApp(config);
