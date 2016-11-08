71d182498046b86a73f89b10068d4de93b16ebf2
Yannick Garcia <yannick.garcia@gmail.com>
2016-11-08 23:43:15 +0100

    test ignore6

--- src/firebase-ref.js
+++ src/firebase-ref.js
@@ -11,7 +11,7 @@ import * as firebase from 'firebase';
  storageBucket: "fifouille-5dccb.appspot.com",
  messagingSenderId: "671212363239"
  };
-/* SUPER 5
+/* SUPER 6
 
 // DB DEV
  const config = {
