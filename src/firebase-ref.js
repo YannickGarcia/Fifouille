26bd06b9f02946ca3cfd0f28097235095e56ee90
Yannick Garcia <yannick.garcia@gmail.com>
2016-11-15 00:40:17 +0100

    db

--- src/firebase-ref.js
+++ src/firebase-ref.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 
-/*
+
  // DB PROD
  const config = {
  apiKey: "AIzaSyCQ-Ki-mSuqsRbqsMfXOUTDySBqYW3xWgs",
@@ -11,8 +11,8 @@ import * as firebase from 'firebase';
  storageBucket: "fifouille-5dccb.appspot.com",
  messagingSenderId: "671212363239"
  };
-*/
 
+/*
 // DB DEV
  const config = {
      apiKey: "AIzaSyBRp1ebUwX98lKMK6ns8i9EEY6M2twy1oo",
@@ -21,7 +21,7 @@ import * as firebase from 'firebase';
      storageBucket: "offifa-dev.appspot.com",
      messagingSenderId: "733443285053"
  };
-
+*/
 
 
 firebase.initializeApp(config);
